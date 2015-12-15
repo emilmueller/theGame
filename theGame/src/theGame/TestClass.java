5a38802fd19de0a40626f6c1c8e8e65f6d440384
emilmueller <mfg@emil-mueller.ch>
2015-12-15 12:22:46 +0100

    v3

--- theGame/src/theGame/TestClass.java
+++ theGame/src/theGame/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Spieler!");
+		System.out.println(TestClass2.getNumber());
 	}
 
 }
