0e5cbd12a97eedd2d2f759a99317a8e0fd241908
DemonicSlitter <alessandro.falco.biella@gmail.com>
2016-01-07 18:18:33 +0100

    hellö

---
+++ The Game/src/Kreatur.java
@@ -0,0 +1,14 @@
+public class Kreatur {
+	private String name;
+	private int dmg;
+	
+	public Kreatur(String name, int dmg){
+		this.name=name;
+		this.strength=strength;
+	}
+	
+	
+	public String getName(){
+		return this.name;
+	}
+}
