9872e7bf9f4ff78fd73a32fe8b0dc3515acacae1
DemonicSlitter <alessandro.falco.biella@gmail.com>
2016-01-07 17:03:36 +0100

    initial

--- The_Game/src/The_Game/Kartennorm.java
+++ The_Game/src/The_Game/Kartennorm.java
@@ -2,8 +2,18 @@ package The_Game;
 
 public class Kartennorm {
 	static public String typ = "Das ist eine leere Kreaturenkarte";
-	
+
 	String name = "normale Kreatur";
-	
+
+	int x;
+	int y;
+	int dmg;
+	int hp;
+	int def;
+	int cost;
+
+	public Kartennorm(String name) {
+		this.name = name;
+	}
 
 }
