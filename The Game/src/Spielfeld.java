1e0ad2697daba49560a8cc71be0d6efd8c7e09bc
SunriseChair <aschmann.dominik@gmail.com>
2016-01-14 17:58:04 +0100

    hh

--- The Game/src/Spielfeld.java
+++ The Game/src/Spielfeld.java
@@ -1,5 +1,18 @@
 import processing.core.PApplet;
 
 public class Spielfeld extends PApplet {
-
+	//Instanz-Felder
+	String name;
+	int y; //y-Ausdehnung(rechts-links)
+	int x; //x-Ausdhnung(hinten-vorne)
+	int z; //z-Ausdehnung("Dicke")
+	
+	//Konstruktor
+	public Feld(String name, int x, int y, int z){
+		this.name=name;
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	
+	}
 }
