4d13901f85ae373fa15f354163b7f29386372ee8
SunriseChair <aschmann.dominik@gmail.com>
2016-01-28 16:52:57 +0100

    l

--- The Game/src/Spielfeld.java
+++ The Game/src/Spielfeld.java
@@ -6,13 +6,27 @@ public class Spielfeld extends PApplet {
 	int y; //y-Ausdehnung(rechts-links)
 	int x; //x-Ausdhnung(hinten-vorne)
 	int z; //z-Ausdehnung("Dicke")
+	int a; //x-Position
+	int b; //y-Position
 	
 	//Konstruktor
-	public Spielfeld(String name, int x, int y, int z){
+	public Spielfeld(String name, int a, int b, int x, int y, int z){
 		this.name=name;
 		this.x=x;
 		this.y=y;
 		this.z=z;
+		this.a=a;
+		this.b=b;
 	
+	}
+	
+	public void print(){
+		public void setup(){
+			frameRate(30);
+		}
+		public void draw(){
+			rect(a+x, b, y, z)
+			line
+		}
 	}
 }
