db86edb72fed913d67604a9f8fbc49dff310c99f
SunriseChair <aschmann.dominik@gmail.com>
2016-04-07 17:44:11 +0200

    Inkl. Card

---
+++ The Game/src/Card.java
@@ -0,0 +1,15 @@
+public class Card {
+	String name; // Name der Karte
+
+	int cost; // Kosten der Karte
+	int number; // nummer der Karte
+
+
+	public Card(String name, int cost, int number) {
+
+		this.name = name;
+		this.cost = cost;
+		this.number = number;
+
+	}
+}
--- The Game/src/Main.java
+++ The Game/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 		Charakter assassinDante = new Charakter ("Dante", 666, 0001, 6, 66, 6, 66);
 		Charakter deathScytheAlessandro = new Charakter ("Alessandro", 500, 0002, 6, 99, 20, 100);
         Charakter picosBoyfriendDominik = new Charakter ("Dominik", 100, 0003, 5, 69, 69, 100);
-//da hesch döme
 	}
 
 }
